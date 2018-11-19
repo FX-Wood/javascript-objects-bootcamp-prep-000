e024479315942b344ae7a68238b5f419083a5073
FX Wood <fxwood2@gmail.com>
2018-11-19 02:22:58 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,7 @@
+var playlist = new Object({['Slowdive']: 'Alison', ['My Bloody Valentine']: 'Sometimes'});
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  Object.assign({}, playlist, {[artistName]: songTitle})
+
+  return playlist
+}
