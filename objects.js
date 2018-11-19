7dc2907d3dee817e9059161e9389c8b9f98c9eb9
FX Wood <fxwood2@gmail.com>
2018-11-19 02:44:41 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -4,3 +4,7 @@ function updatePlaylist(playlist, songTitle, artistName) {
   playlist[songTitle] = artistName
   return playlist
 }
+
+function removeFromPlaylist(playlist, songTitle) {
+  delete playlist.songTitle
+}
